16595bc2b7606c9cabbcfe30f35c8ce24a3ad002
abravosek <103817005+abravosek@users.noreply.github.com>
2022-04-20 19:38:11 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showString("Hola, somos...")
+basic.pause(500)
+while (true) {
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+}
